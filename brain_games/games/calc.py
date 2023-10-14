d27fd25e319de5c703e7a7402b3870424961e6d5
Denis <ad.way@mail.ri>
2023-10-14 14:32:46 +0400

    fix10

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,11 +7,12 @@ RULES = RULES["calc"]
 def game_logic():
     first_num = randint(0, 9)
     last_num = randint(0, 9)
-    operators = {'+': first_num + last_num, '-': first_num - last_num, '*': first_num * last_num}
+    operators = {'+': first_num + last_num, '-': first_num - last_num,
+                 '*': first_num * last_num
+                 }
 
     action = choice(list(operators.keys()))
     expression = f'{first_num} {action} {last_num}'
     win_txt = str(operators[action])
 
     return win_txt, expression
-
