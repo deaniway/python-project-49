a056feed3f6924c1763e51364e731dc6b936e6d2
Denis <ad.way@mail.ri>
2023-10-17 18:13:40 +0400

    fix return txt

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,24 +1,27 @@
-from brain_games.cli import welcome_user
+import prompt
 
 
 def run_game(get_question_and_answer, instruction):
-    user = welcome_user()
 
+    user_name = prompt.string('Welcome to the Brain Games!\n'
+                              'May I have your name? ')
+    print(f"Hello, {user_name}")
     print(instruction)
 
-    con = 0
-    for _ in range(3):
+    max_round_game = 3
+
+    for _ in range(max_round_game):
         question, correct_answer = get_question_and_answer()
         print(f'Question: {question}')
-        user_answer = input('Your answer: ')
+        user_answer = prompt.string('Your answer: ')
 
         if user_answer == correct_answer:
             print('Correct!')
-            con += 1
+
         else:
             print(f"'{user_answer}' is wrong answer ;(."
                   f" Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {user}!")
-            break
-    if con == 3:
-        print(f'Congratulations, {user}!')
+            print(f"Let's try again, {user_name}!")
+            return
+
+    print(f'Congratulations, {user_name}!')
