3a8cd2b083d98ae678dfb46e4928f397d0f44f20
Denis <ad.way@mail.ri>
2023-12-13 11:42:49 +0400

    finish 1.2

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,17 +1,15 @@
 from brain_games.utils import generate_number, generate_random_number
+from brain_games.utils import generate_progression
 from brain_games.constants import GAME_INSTRUCTIONS, PROGRESSION_LENGTH
 from brain_games.engine import run_game
-
-
-def generate_progression(start, step, length):
-    return list(range(start, start + step * length, step))
 
 
 def calculate_expression_and_result():
     start_num = generate_random_number()
     step = generate_random_number()
+    lenght = PROGRESSION_LENGTH
 
-    progression = generate_progression(start_num, step, PROGRESSION_LENGTH)
+    progression = generate_progression(start_num, step, lenght)
 
     index_to_replace = generate_number(len(progression))
     correct_answer = progression[index_to_replace]
--- brain_games/utils.py
+++ brain_games/utils.py
@@ -2,8 +2,12 @@ import random
 
 
 def generate_random_number():
-    return random.randint(0, 9)
+    return random.randint(1, 10)
 
 
 def generate_number(num):
     return random.randrange(num)
+
+
+def generate_progression(start, step, length):
+    return list(range(start, start + step * length, step))
